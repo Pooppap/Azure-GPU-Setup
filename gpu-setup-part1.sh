94aee52d20c1bcbb01120a109318360ba96bf635
Pooppap <Chaiyawan38@gmail.com>
2017-07-08 17:06:39 +0100

    Update gpu-setup-part1.sh

--- gpu-setup-part1.sh
+++ gpu-setup-part1.sh
@@ -10,14 +10,15 @@ mkdir -p $SETUP_DIR
 cd $SETUP_DIR
 
 # install python libraries
-sudo apt-get -y install python3-numpy python3-dev python3-wheel python3-mock python3-matplotlib python3-pip
+sudo apt-get -y install python3-numpy python3-dev python3-wheel python3-mock python3-matplotlib python3-pip nvidia-cuda-toolkit
 
 # install cuda drivers
-if [ ! -f "cuda_8.0.61_375.26_linux-run" ]; then 
-	wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
+if [ ! -f "cuda-repo-ubuntu1604_8.0.61-1_amd64.deb" ]; then 
+	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 fi
-chmod +x cuda_8.0.61_375.26_linux-run
-sudo sh cuda_8.0.61_375.26_linux-run --silent --verbose --driver
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo apt-get update
+sudo apt-get -y install cuda
 
 echo "Setup requires a reboot to continue."
 echo "The VM will reboot now. Login after it restarts and continue installation from part2."
